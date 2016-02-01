6f60864bc25d7328640e63821eb42d826d7a1e63
XMFraker <3057600441@qq.com>
2016-02-01 18:15:50 +0800

    增加XMNPhotoPicker - 模仿QQ选择图片功能

---
+++ XMNPhotoPickerKit/Views/XMNPhotoPicker.h
@@ -0,0 +1,13 @@
+//
+//  XMNPhotoPicker.h
+//  XMNPhotoPickerKitExample
+//
+//  Created by XMFraker on 16/2/1.
+//  Copyright © 2016年 XMFraker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMNPhotoPicker : UIView
+
+@end
---
+++ XMNPhotoPickerKit/Views/XMNPhotoPicker.m
@@ -0,0 +1,21 @@
+//
+//  XMNPhotoPicker.m
+//  XMNPhotoPickerKitExample
+//
+//  Created by XMFraker on 16/2/1.
+//  Copyright © 2016年 XMFraker. All rights reserved.
+//
+
+#import "XMNPhotoPicker.h"
+
+@implementation XMNPhotoPicker
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
